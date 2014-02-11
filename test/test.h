f7f1b89e10060c80e150fe027b936b8c169ce0b5
I7ZI <I7ZI@hotmail.com>
2014-02-11 11:50:33 -0700

    Added fog parser to cxxtest - likely need ply package to run

--- test/test.h
+++ test/test.h
@@ -5,8 +5,7 @@
 #include <iostream>
 #include <fstream>
 
- std::ofstream log_file("J3NI_test_log.log");
-//using namespace IpmiCommandDefines;
+std::ofstream log_file("J3NI_test_log.log");
 class J3NITestSuite : public CxxTest::TestSuite 
 {
     public:
@@ -22,9 +21,19 @@ class J3NITestSuite : public CxxTest::TestSuite
     
     void testIsPing(void) {
         IpmiMessage testMsg(IpmiCommandDefines::PING_REQUEST, 12);
+        TS_TRACE("Executing testIsPing");
         TS_ASSERT(MsgHandler::isPing(testMsg));
     }
-    
+
+    void testPong(void) {
+        IpmiMessage req(IpmiCommandDefines::PING_REQUEST, 12);
+        const unsigned char* msg = IpmiCommandDefines::PONG_RESPONSE;
+        IpmiMessage testMsg;
+        MsgHandler::pong(req, testMsg);
+        TS_ASSERT_SAME_DATA(testMsg.message(), msg, 27);
+        TS_ASSERT_EQUALS(testMsg.message()[9], req.message()[9]);
+    }
+   
     
 };
 
