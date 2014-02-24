b9a625d7d30564994173714dce60e0b171b544f1
jcheu <aznsteamedbread@hotmail.com>
2014-02-24 03:38:44 -0700

    New Test

--- test/test.h
+++ test/test.h
@@ -15,6 +15,7 @@ class J3NITestSuite : public CxxTest::TestSuite
     }
 
     void testIpmiMessageValidCreation(void) {
+	TS_TRACE("Executing testIpmiMessageValidCreation");
         const unsigned char* msg = IpmiCommandDefines::PONG_RESPONSE;
         IpmiMessage * testMsg = new IpmiMessage(msg, 27);
 
@@ -30,12 +31,24 @@ class J3NITestSuite : public CxxTest::TestSuite
     }
 
     void testPong(void) {
+	TS_TRACE("Executing testPong");
         IpmiMessage req(IpmiCommandDefines::PING_REQUEST, 12);
         const unsigned char* msg = IpmiCommandDefines::PONG_RESPONSE;
         IpmiMessage testMsg;
         MsgHandler::pong(req, testMsg);
         TS_ASSERT_SAME_DATA(testMsg.message(), msg, 27);
         TS_ASSERT_EQUALS(testMsg.message()[9], req.message()[9]);
+    }
+
+   void testChassis(void){
+	TS_TRACE("Executing testChassis");
+	IpmiMessage req(IpmiCommandDefines::GET_CHASSIS_STATUS,21);
+	int powerOn = 0x61;
+	int lastpowerEvent = 0x00;
+	int chassisState = 0x00;
+	IpmiMessage testChassisStatus;
+	MsgHandler::processRequest(req, testChassisStatus);
+	TS_ASSERT(req.message()[19] == 0x01);
     }
    
     
