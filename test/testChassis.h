9f573f829901af439a2c137df0b539e6e14312ec
I7ZI <I7ZI@hotmail.com>
2014-03-07 10:56:29 -0700

    Added Chassis identify test, and some traces

--- test/testChassis.h
+++ test/testChassis.h
@@ -9,7 +9,6 @@ class TestChassisSuite : public CxxTest::TestSuite
 {
     private:
     unsigned char* blank_request;
-    unsigned char* response;
     public:
     
     static TestChassisSuite* createSuite() {
@@ -24,15 +23,14 @@ class TestChassisSuite : public CxxTest::TestSuite
     
     void setUp(){
         blank_request = new unsigned char[IpmiCommandDefines::MAX_DATA_SIZE];
-        response = new unsigned char[IpmiCommandDefines::MAX_DATA_SIZE];
     }
     
     void tearDown() {
         delete blank_request;
-        delete response;
     }
     
     void testGetChassisCapab_NoBridge(void) {
+        TS_TRACE("Testing Chassis Capabilities CMD -- default (no bridge) return expected");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x00;
         IpmiMessage request(blank_request, 21);
         IpmiMessage testResponse;
@@ -42,6 +40,7 @@ class TestChassisSuite : public CxxTest::TestSuite
     }
 
     void testGetChassisStatus(void) {
+        TS_TRACE("Testing Get Chassis Status CMD -- default return expected");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
         IpmiMessage request(blank_request, 21);
         IpmiMessage testResponse;
@@ -49,18 +48,19 @@ class TestChassisSuite : public CxxTest::TestSuite
         
         unsigned char powerState = 0x61;
         unsigned char lastPower = 0x00;
-        unsigned char miscChassis = 0x00;
+        unsigned char miscChassisMask = 0xBF;
         
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
         TS_ASSERT_EQUALS(testResponse.length(), 25);
 
         TS_ASSERT_EQUALS(testResponse.data()[1], powerState);
         TS_ASSERT_EQUALS(testResponse.data()[2], lastPower);
-        TS_ASSERT_EQUALS(testResponse.data()[3], miscChassis);
+        TS_ASSERT_EQUALS(testResponse.data()[3] & miscChassisMask, 0x00);
 
     }
     
     void testChassisControl_PowerOff(void) {
+        TS_TRACE("Testing Chassis Control CMD with power off request -- 'not allowed in current state' error expected");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x00;
         IpmiMessage request(blank_request, 22);
@@ -68,7 +68,7 @@ class TestChassisSuite : public CxxTest::TestSuite
         IpmiMessage testResponse;
         MsgHandler::processRequest(request, testResponse);
         TS_ASSERT_EQUALS(testResponse[IpmiCommandDefines::DATA_START_INDEX], IpmiCommandDefines::COMP_CODE_OK);
-
+        TS_TRACE("\tChecking power off status");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
         IpmiMessage request2(blank_request, 21);
         MsgHandler::processRequest(request2, testResponse);
@@ -76,6 +76,7 @@ class TestChassisSuite : public CxxTest::TestSuite
     }
     
     void testChassisControl_InvalidCycle(void) {
+        TS_TRACE("Testing Chassis Control CMD with cycle request in powered off state -- 'not allowed in current state' error expected");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x02;
         IpmiMessage request(blank_request, 22);
@@ -86,6 +87,7 @@ class TestChassisSuite : public CxxTest::TestSuite
     }
 
     void testChassisControl_InvalidReset(void) {
+        TS_TRACE("Testing Chassis Control CMD with reset request in powered off state");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x03;
         IpmiMessage request(blank_request, 22);
@@ -96,6 +98,7 @@ class TestChassisSuite : public CxxTest::TestSuite
     }
     
     void testChassisControl_PowerOn(void) {
+        TS_TRACE("Testing Chassis Control CMD with power on request");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x01;
         IpmiMessage request(blank_request, 22);
@@ -104,19 +107,19 @@ class TestChassisSuite : public CxxTest::TestSuite
         MsgHandler::processRequest(request, testResponse);
         TS_ASSERT_EQUALS(testResponse[IpmiCommandDefines::DATA_START_INDEX], IpmiCommandDefines::COMP_CODE_OK);
         
+        TS_TRACE("\tChecking power on status, last event, etc.");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
         IpmiMessage request2(blank_request, 21);
         MsgHandler::processRequest(request2, testResponse);
-
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
         TS_ASSERT_EQUALS(testResponse.length(), 25);
-        
         TS_ASSERT_EQUALS(testResponse.data()[1], 0x61);
         TS_ASSERT_EQUALS(testResponse.data()[2], 0x10);
-        TS_ASSERT_EQUALS(testResponse.data()[3], 0x00);
+        TS_ASSERT_EQUALS(testResponse.data()[3] & 0xBF, 0x00);
     }
     
     void testChassisControl_Cycle(void) {
+        TS_TRACE("Testing Chassis Control CMD with power cycle request in powered on state");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x02;
         IpmiMessage request(blank_request, 22);
@@ -125,23 +128,24 @@ class TestChassisSuite : public CxxTest::TestSuite
         MsgHandler::processRequest(request, testResponse);
         TS_ASSERT_EQUALS(testResponse[IpmiCommandDefines::DATA_START_INDEX], IpmiCommandDefines::COMP_CODE_OK);
         
+        TS_TRACE("\tChecking power on status, last event, etc.");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
         IpmiMessage request2(blank_request, 21);
         MsgHandler::processRequest(request2, testResponse);
-        
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
         TS_ASSERT_EQUALS(testResponse.length(), 25);
-        
         TS_ASSERT_EQUALS(testResponse.data()[1], 0x61);
         TS_ASSERT_EQUALS(testResponse.data()[2], 0x00);
-        TS_ASSERT_EQUALS(testResponse.data()[3], 0x00);
+        TS_ASSERT_EQUALS(testResponse.data()[3] & 0xBF, 0x00);
         
+        TS_TRACE("\tChecking reset cause");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x07;
         IpmiMessage request3(blank_request, 21);
         MsgHandler::processRequest(request3, testResponse);
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
         TS_ASSERT_EQUALS(testResponse.data()[1], 0x01);
         
+        TS_TRACE("\tChecking successful POH reset");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x0F;
         IpmiMessage request4(blank_request, 21);
         MsgHandler::processRequest(request4, testResponse);
@@ -150,6 +154,7 @@ class TestChassisSuite : public CxxTest::TestSuite
     }
     
     void testChassisControl_Reset(void) {
+        TS_TRACE("Testing Chassis Control CMD with reset request in powered on state");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x02;
         blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x03;
         IpmiMessage request(blank_request, 22);
@@ -158,30 +163,119 @@ class TestChassisSuite : public CxxTest::TestSuite
         MsgHandler::processRequest(request, testResponse);
         TS_ASSERT_EQUALS(testResponse[IpmiCommandDefines::DATA_START_INDEX], IpmiCommandDefines::COMP_CODE_OK);
         
+        TS_TRACE("\tChecking power on status, last event, etc.");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
         IpmiMessage request2(blank_request, 21);
-        MsgHandler::processRequest(request2, testResponse);
-        
+        MsgHandler::processRequest(request2, testResponse);        
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
-        TS_ASSERT_EQUALS(testResponse.length(), 25);
-        
+        TS_ASSERT_EQUALS(testResponse.length(), 25);        
         TS_ASSERT_EQUALS(testResponse.data()[1], 0x61);
         TS_ASSERT_EQUALS(testResponse.data()[2], 0x00);
-        TS_ASSERT_EQUALS(testResponse.data()[3], 0x00);
+        TS_ASSERT_EQUALS(testResponse.data()[3] & 0xBF, 0x00);
         
+        TS_TRACE("\tChecking reset cause");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x07;
         IpmiMessage request3(blank_request, 21);
         MsgHandler::processRequest(request3, testResponse);
         TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
         TS_ASSERT_EQUALS(testResponse.data()[1], 0x01);
         
+        TS_TRACE("\tChecking successful POH reset");
         blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x0F;
         IpmiMessage request4(blank_request, 21);
         MsgHandler::processRequest(request4, testResponse);
         unsigned char expectedPOH[6] = {0x00, 0x3c, 0x00, 0x00, 0x00, 0x00};
         TS_ASSERT_SAME_DATA(testResponse.data(), expectedPOH, 6);
     }
+   
+   void testChassisReset(void){
+        TS_TRACE("Testing Chassis Reset CMD --'disabled' error code expected");
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x03;
+        IpmiMessage request(blank_request, 21);
+        IpmiMessage testResponse;
+        MsgHandler::processRequest(request, testResponse);
+
+        TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::CANNOT_EXEC_DISABLED);
+        TS_ASSERT_EQUALS(testResponse.length(), 22);
+   }
+
+   void testChassisIdentify_NoArgs(void){
+       TS_TRACE("Testing Chassis Identify CMD with no arguments");
+       blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x04;
+       IpmiMessage request(blank_request, 21);
+       IpmiMessage testResponse;
+       MsgHandler::processRequest(request, testResponse);
+
+       TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
+       
+       blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
+       IpmiMessage request2(blank_request, 21);
+       MsgHandler::processRequest(request2, testResponse);
+       if ((testResponse.data()[3] & 0x40) == 0x40){
+           TS_TRACE("\tIdentify status supported; Checking misc. state - timed identify");
+           TS_FAIL("No timed identify -- TO DO: check 15 sec identify interval");
+       }
+   }
     
+    void testChassisIdentify_IntervalArg(void){
+        TS_TRACE("Testing Chassis Identify CMD with (optional) interval request byte");
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x04;
+        blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x01;
+        IpmiMessage request(blank_request, 21);
+        IpmiMessage testResponse;
+        MsgHandler::processRequest(request, testResponse);
+        
+        TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
+        
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
+        IpmiMessage request2(blank_request, 21);
+        MsgHandler::processRequest(request2, testResponse);
+        if ((testResponse.data()[3] & 0x40 )== 0x40){
+            TS_TRACE("\tIdentify status supported; Checking misc. state - timed identify");
+            TS_FAIL("No timed identify -- TO DO: check specified identify interval");
+        }
+        
+    }
+    
+    void testChassisIdentify_Off(void){
+        TS_TRACE("Testing Chassis Identify CMD with off request");
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x04;
+        blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x00;
+        IpmiMessage request(blank_request, 21);
+        IpmiMessage testResponse;
+        MsgHandler::processRequest(request, testResponse);
+        
+        TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
+        
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
+        IpmiMessage request2(blank_request, 21);
+        MsgHandler::processRequest(request2, testResponse);
+        if ((testResponse.data()[3] & 0x40 )== 0x40){
+            TS_TRACE("\tIdentify status supported; Checking misc. state - identify off");
+            TS_ASSERT_EQUALS(testResponse.data()[3] & 0x70, 0x40);
+        }
+    }
+    
+    void testChassisIdentify_ForceArg(void){
+        TS_TRACE("Testing Chassis Identify CMD with (optional) force identify request byte");
+        TS_WARN("Actual BMC may return an error if the 'force' option is not supported");
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x04;
+        blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x01;
+        blank_request[IpmiCommandDefines::DATA_START_INDEX] = 0x01;
+        IpmiMessage request(blank_request, 21);
+        IpmiMessage testResponse;
+        MsgHandler::processRequest(request, testResponse);
+        
+        TS_ASSERT_EQUALS(testResponse.data()[0], IpmiCommandDefines::COMP_CODE_OK);
+        
+        blank_request[IpmiCommandDefines::COMMAND_INDEX] = 0x01;
+        IpmiMessage request2(blank_request, 21);
+        MsgHandler::processRequest(request2, testResponse);
+        if ((testResponse.data()[3] & 0x40) == 0x40){
+            TS_TRACE("\tIdentify status supported; Checking misc. state - indefinite identify");
+            TS_ASSERT_EQUALS(testResponse.data()[3] & 0x70, 0x60);
+        }
+    }
 };
 
 
